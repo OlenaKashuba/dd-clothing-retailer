d1b1a8fcff773ae4e5a9a72546d1ce7f056ebbbf
OlenaKashuba <amely.jazz@gmail.com>
2019-01-20 19:10:50 +0000

    Add test for number of items

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 import shopData from './shop-data.json';
 import vouchersData from './vouchers-data.json';
@@ -25,5 +25,23 @@ describe('App component', () => {
   it('should render the same amount of items compared to json file', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Item').length).toEqual(shopData.items.length);
+  });
+
+  it('Increments number of items in cart when Add button is clicked', () => {
+    const wrapper = mount(<App />);
+    wrapper
+      .find('.add-btn')
+      .first()
+      .simulate('click');
+    expect(wrapper.state('numOfItems')).toEqual(1);
+  });
+
+  it('Get price of item when Add button is clicked', () => {
+    const wrapper = mount(<App />);
+    wrapper
+      .find('.add-btn')
+      .first()
+      .simulate('click');
+    expect(parseInt(wrapper.state('totalPrice'))).toEqual(shopData.items[0].price);
   });
 });
